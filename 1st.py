841b59578646c3042b9ab58f2078a2f21dbcf7f9
21PW33@cs2k16.amcs.ws2k12.psgcs.edu <21PW33@cs2k16.amcs.ws2k12.psgcs.edu>
2022-05-12 18:01:31 +0530

    Created GET and POST request

--- 1st.py
+++ 1st.py
@@ -1,6 +1,5 @@
-                #BACKENED SERVER
 
-from flask import Flask,request
+from flask import Flask, request
 app=Flask(__name__)
 students={
             '1':{"Name":"Shyam","Rollno":"21pw01","Course":"Software systems"},
@@ -14,13 +13,34 @@ students={
             '9':{"Name":"Akshit","Rollno":"21pw09","Course":"Software systems"},
             '10':{"Name":"Varun","Rollno":"21pw10","Course":"Software systems"},
         }
-@app.route('/students')
-def display():
-    return students
-@app.route('/students<id>')
-def displayrollno(id):
-    id=request.view_args['id']
+@app.route('/students', methods=['POST', 'GET'])
+def get_all_students():
+    if request.method=='GET':
+        return students
+    else:
+        name = request.form['Name']
+        roll_no = request.form['Rollno']
+        course = request.form['Course']
+        students['11'] = {'Name': name, 'Rollno': roll_no, 'Course': course}
+        print(students)
+        return students
+
+@app.route('/students/<id>')
+def get_student_by_id(id):
     return students[id]
+
+@app.route('/students/<id>/name')
+def get_student_name_by_id(id):
+    return students[id]["Name"]
+
+@app.route('/students/names')
+def get_all_students_name():
+    student_name_list=[]
+    for index, student in students.items():
+        student_name_list.append(student["Name"])
+    return str(student_name_list)
+
+
 
 if __name__=='__main__':
     app.run()
